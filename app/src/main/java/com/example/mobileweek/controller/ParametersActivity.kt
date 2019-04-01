080ae7318ca09fdfae36f2b64cd930b3a4c58610
Tynyndil <thomasraineau3@gmail.com>
2019-04-01 18:15:25 +0200

    Screens et TabBarNavigation

---
+++ app/src/main/java/com/example/mobileweek/controller/ParametersActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mobileweek.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobileweek.R
+
+class ParametersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
