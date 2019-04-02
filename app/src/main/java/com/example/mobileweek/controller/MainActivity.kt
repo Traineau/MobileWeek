00264cfc2b71ce67f458824003a9882b88578d6d
Tynyndil <thomasraineau3@gmail.com>
2019-04-02 15:37:21 +0200

    Removing Maps from Home

--- app/src/main/java/com/example/mobileweek/controller/MainActivity.kt
+++ app/src/main/java/com/example/mobileweek/controller/MainActivity.kt
@@ -9,18 +9,9 @@ import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBar
 import android.widget.Button
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
 
     lateinit var toolbar: ActionBar
-    private lateinit var mMap: GoogleMap
-    private var scanButton: Button? = null
 
     private val OnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -61,19 +52,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         bottomNavigation.setOnNavigationItemSelectedListener(OnNavigationItemSelectedListener)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     private fun openFragment(fragment: Fragment) {
--- app/src/main/java/com/example/mobileweek/controller/ScanActivity.kt
+++ app/src/main/java/com/example/mobileweek/controller/ScanActivity.kt
@@ -2,64 +2,15 @@ package com.example.mobileweek.controller
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import com.example.mobileweek.R
 
 class ScanActivity : AppCompatActivity() {
 
-    lateinit var toolbar: ActionBar
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                goToHome()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_play -> {
-                goToPlay()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_profil -> {
-                goToProfil()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_parameters -> {
-                goToParameters()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-
-        toolbar = supportActionBar!!
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
-
-        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    private fun goToHome(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun goToPlay(){
-        val intent = Intent(this, PlayFragment::class.java)
-        startActivity(intent)
-    }
-
-    private fun goToParameters(){
-        val intent = Intent(this, ParametersFragment::class.java)
-        startActivity(intent)
-    }
-
-    private fun goToProfil(){
-        val intent = Intent(this, ProfilFragment::class.java)
-        startActivity(intent)
-    }
 }
