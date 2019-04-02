8cbf6b0601fff151383b227628bea15433f27f57
Tynyndil <thomasraineau3@gmail.com>
2019-04-02 13:57:07 +0200

    NavBar Correction + Scan screen

--- app/src/main/java/com/example/mobileweek/controller/ParametersActivity.kt
+++ app/src/main/java/com/example/mobileweek/controller/ParametersActivity.kt
@@ -35,7 +35,7 @@ class ParametersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_parameters)
 
         toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
