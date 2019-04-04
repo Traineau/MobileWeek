82436e2556a16f6a9ba0822244da54d2c7095bf6
Tynyndil <thomasraineau3@gmail.com>
2019-04-04 02:01:02 +0200

    Working app + Home feed

--- app/src/main/java/com/example/mobileweek/controller/GameActivity.kt
+++ app/src/main/java/com/example/mobileweek/controller/GameActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobileweek.controller
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -21,18 +22,22 @@ import android.widget.ImageView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.squareup.picasso.Picasso
 import android.location.Geocoder
+import android.text.Html
 import android.util.Log
+import android.view.View
 import com.google.android.gms.maps.model.*
 import java.util.*
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlin.collections.ArrayList
+import kotlin.math.*
 
 
 class GameActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var mMap: GoogleMap
-    val product = Product()
-    val myMarkers: MutableList<Marker> = mutableListOf<Marker>()
+    var product = Product()
+    var myMarkers: MutableList<Marker> = mutableListOf<Marker>()
+    var distance: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -96,30 +101,60 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback{
     }
 
     // When the user click on validate
-    protected fun handleValidateResponse(map: GoogleMap){
-        // Addin response marker
-        val newMarkerCoordinate = LatLng(product.lat as Double, product.long as Double)
+    protected fun handleValidateResponse(map: GoogleMap) =// If the first marker wasn't placed
+        if(myMarkers.getOrNull(0) == null) {
+            val alertDialog: AlertDialog? = this.let {
+                val builder = AlertDialog.Builder(it)
+                builder.setMessage(R.string.dialog_no_marker_message)?.setTitle(R.string.dialog_no_marker_title)
+                builder.create()
+                builder.show()
+            }
+        }else{
+            // Addin response marker
+            val newMarkerCoordinate = LatLng(product.lat as Double, product.long as Double)
 
-        val newMarker = map.addMarker(
-            MarkerOptions()
-                .position(newMarkerCoordinate)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+            val newMarker = map.addMarker(
+                MarkerOptions()
+                    .position(newMarkerCoordinate)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            );
 
-        // Putting marker in the list
-        myMarkers.add(1, newMarker)
+            // Putting marker in the list
+            myMarkers.add(1, newMarker)
 
-        // Creating a new LatLngBounds with both markers to center the view
-        val newPosition = LatLngBounds.builder().include(myMarkers[0].position).include(myMarkers[1].position).build()
-        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(newPosition, 20))
+            // Creating a new LatLngBounds with both markers to center the view
+            val newPosition =
+                LatLngBounds.builder().include(myMarkers[0].position).include(myMarkers[1].position).build()
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(newPosition, 200))
 
-        // Adding a red path between the two markers
-        mMap.addPolyline(PolylineOptions()
-            .add( myMarkers[0].position, myMarkers[1].position)
-            .width(5F)
-            .color(Color.RED))
+            // Adding a red path between the two markers
+            mMap.addPolyline(
+                PolylineOptions()
+                    .add(myMarkers[0].position, myMarkers[1].position)
+                    .width(5F)
+                    .color(Color.RED)
+            )
 
-        // Calculating the km between the two markers
-    }
+            // Calculating the km between the two markers
+            distance = getDistanceFromLatLonInkm(
+                myMarkers[0].position.latitude,
+                myMarkers[0].position.longitude,
+                myMarkers[1].position.latitude,
+                myMarkers[1].position.longitude
+            ).toInt()
+
+            val points = 1000 - distance
+
+            estimationText.setText("Votre estimation est à $distance km du lieu d'origine du produit scanné !")
+            winPointsText.text = "Vous avez gagné $points points !"
+            responseLayout.visibility = View.VISIBLE
+
+            validateResponse.text = "Terminé"
+            validateResponse.setOnClickListener {
+                val intent = Intent(this@GameActivity, MainActivity::class.java);
+                startActivity(intent);
+            }
+        }
 
     protected fun getGeocodeInfo(adress: String?): List<android.location.Address>{
         val TAG = "Error finding the city"
@@ -186,7 +221,19 @@ class GameActivity : AppCompatActivity(), OnMapReadyCallback{
     }
 
     fun getDistanceFromLatLonInkm(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double{
-        return 200.0
+        // Radius of the earth
+        val R = 6371
+        var dLat = deg2rad(lat2-lat1)
+        val dlon = deg2rad(lon2-lon1)
+        val a = sin(dLat/2) * sin(dLat/2) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dlon/2) * sin(dlon/2)
+        val b = 2 * atan2(sqrt(a), sqrt(1-a))
+        val c = R * b
+
+        return c
+    }
+
+    fun deg2rad(deg: Double): Double{
+        return deg * (PI/180)
     }
 
 }
