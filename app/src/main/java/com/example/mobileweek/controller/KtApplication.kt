07a472372c6feaeb111b4a4f6fad5ccc09459937
Tynyndil <thomasraineau3@gmail.com>
2019-04-04 16:04:37 +0200

    Scan, correction bug

---
+++ app/src/main/java/com/example/mobileweek/controller/KtApplication.kt
@@ -0,0 +1,9 @@
+package com.example.mobileweek.controller
+
+import android.support.multidex.MultiDexApplication
+
+class KtApplication: MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
